91301095d54ae3bbe74b83ed2ddf691d1dd425a7
Deoval Junior <deovaljr@gmail.com>
2016-11-14 22:37:21 -0200

    cp funcionando sem opções

--- cp.c
+++ cp.c
@@ -13,34 +13,40 @@
 
 #define MODO_CRIACAO S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
 
-/*int a_flag = 0;
-int m_flag = 0;
-char buffer[80]; // ajuda para usar o sprintf*/
-
 void copy_file(char *source_file_name, char *dest_file_name){
-  int file_source, file_dest;
-  int bytes_lidos;
-  char *buffer;
-  struct stat st;
+	int file_source, file_dest;
+	int bytes_lidos;
+	char *buffer;
+	struct stat st;
+
 	stat(source_file_name, &st);
 
-  buffer = malloc(st.st_size*sizeof(char));
+	buffer = malloc(st.st_size*sizeof(char));
 
-	if((file_source = open(source_file_name,O_RDONLY, S_IREAD))==-1){
-        fprintf(stderr,"Erro: %s\n",strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    
-  if((file_dest = open(dest_file_name,O_WRONLY | O_TRUNC | O_CREAT, MODO_CRIACAO))==-1){
-      fprintf(stderr,"Erro: %s\n",strerror(errno));
-      exit(EXIT_FAILURE);
-  }
+	file_source = open(source_file_name,O_RDONLY, S_IREAD);
+	if(file_source == -1){
+	    fprintf(stderr,"Erro: %s\n",strerror(errno));
+	    exit(EXIT_FAILURE);
+	}
 
-  while((bytes_lidos=read(file_source,buffer,st.st_size))!=0) 
-       write(file_dest,buffer,bytes_lidos);
+	file_dest = open(dest_file_name,O_WRONLY | O_TRUNC | O_CREAT, MODO_CRIACAO);
+	if(file_dest == -1){
+		char dest_buff[1000] = "";
+		strcat(dest_buff, dest_file_name);
+		strcat(dest_buff, "/");
+		strcat(dest_buff, source_file_name);
+		file_dest = open(dest_buff,O_WRONLY | O_TRUNC | O_CREAT, MODO_CRIACAO);
+		if (file_dest == -1){
+			fprintf(stderr,"Erroaaa: %s\n",strerror(errno));
+	  		exit(EXIT_FAILURE);
+		}	
+	}
 
-  close(file_source);
-  close(file_dest);
+	while((bytes_lidos=read(file_source,buffer,st.st_size))!=0) 
+	   write(file_dest,buffer,bytes_lidos);
+
+	close(file_source);
+	close(file_dest);
 }
 
 void help(){
@@ -58,10 +64,10 @@ int main(int argc, char **argv){
 	while((c = getopt(argc, argv, "am")) != -1){			
 		switch(c){		
 			case 'a':
-				a_flag = 1;
+				//a_flag = 1;
 				break;
 			case 'm': 
-				m_flag = 1;
+				//m_flag = 1;
 				break;
 			default:
 			    printf("./cp: Parametro desconhecido: %c\n", optopt);
@@ -79,20 +85,22 @@ int main(int argc, char **argv){
 		exit(EXIT_FAILURE);
 	}
 
-	copy_file(argv[optind], argv[optind+1]);
-
-	
-	/*for(i = optind; i < argc; i++) {
-	    char* file_name = argv[i];
-	    //TO DO Verificar permissões na criação do arquivo
-	    file = open(file_name, O_CREAT, S_IRUSR | S_IWOTH);
-	    if(file == -1) {
-	    	sprintf(buffer, "./touch %s", file_name);
-	    	perror(buffer);
-	    	exit(EXIT_FAILURE);
-	    }
-	    
-	}*/
-		
+	if ((optind+2) == argc){
+		copy_file(argv[optind], argv[optind+1]);	
+	}
+	else{
+		struct stat st;
+		if((stat(argv[argc-1], &st))==-1){
+			printf( "[*] Can't stat() file %s\n", argv[argc-1] );
+      		exit(EXIT_FAILURE);
+		}
+		if (st.st_mode != S_IFDIR){
+			printf("./cp: target “%s” is not a directory\n", argv[argc-1]);
+			exit(EXIT_FAILURE);
+		}
+		for (i = optind; i < (argc - 1); i++){
+			copy_file(argv[i], argv[argc-1]);
+		}		
+	}		
 	return 0;
 }
